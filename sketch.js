62a29a3c39c0c4d0f82bc61547e4bc35150228ab
David Katko <davidkatko@gmail.com>
2023-04-11 23:35:02 -0400

    functions is_user_raising_hand, is_user_t_posing

--- sketch.js
+++ sketch.js
@@ -1,38 +1,92 @@
 // Adapted from https://p5js.org/examples/interaction-snake-game.html
 //
-var host = "localhost:4444";
+let host = "cpsc484-04.yale.internal:8888";
 $(document).ready(function() {
   frames.start();
   twod.start();
 });
 
-var frames = {
+let frames = {
   socket: null,
 
   start: function() {
-    var url = "ws://" + host + "/frames";
+    let url = "ws://" + host + "/frames";
     frames.socket = new WebSocket(url);
     frames.socket.onmessage = function (event) {
-      var command = frames.get_left_wrist_command(JSON.parse(event.data));
+      // let user_raising_hand = frames.is_user_raising_hand(JSON.parse(event.data));
+      // let user_t_posing = frames.is_user_t_posing(JSON.parse(event.data));
+
+      let command = frames.get_left_wrist_command(JSON.parse(event.data));
       if (command !== null) {
         sendWristCommand(command);
       }
     }
   },
 
+  is_user_raising_hand: function (frame) {
+    if (frame.people.length < 1) {
+      return false; // no people in the frame
+    }
+
+    // Normalize by subtracting the root (pelvis) joint coordinates
+    let pelvis_x = frame.people[0].joints[0].position.x;
+    let pelvis_y = frame.people[0].joints[0].position.y;
+    // let pelvis_z = frame.people[0].joints[0].position.z;
+
+    let hand_left_x = (frame.people[0].joints[8].position.x - pelvis_x) * -1;
+    let hand_left_y = (frame.people[0].joints[8].position.y - pelvis_y) * -1;
+    // let hand_left_z = (frame.people[0].joints[8].position.z - pelvis_z) * -1; // should we use this at all?
+
+    let hand_right_x = (frame.people[0].joints[15].position.x - pelvis_x) * -1;
+    let hand_right_y = (frame.people[0].joints[15].position.y - pelvis_y) * -1;
+    // let hand_right_z = (frame.people[0].joints[15].position.z - pelvis_z) * -1; // should we use this at all?
+
+    let left_hand_raised = hand_left_x < 200 && hand_left_x > -200 && hand_left_y > 500;
+    let right_hand_raised = hand_right_x < 200 && hand_right_x > -200 && hand_right_y > 500;
+    if (left_hand_raised || right_hand_raised) {
+      console.log("hand(s) raised!");
+      return true;
+    }
+
+    return false; // no hands up
+  },
+
+  is_user_t_posing: function (frame) {
+    if (frame.people.length < 1) {
+      return false;
+    }
+
+    // Normalize by subtracting the root (pelvis) joint coordinates
+    let pelvis_x = frame.people[0].joints[0].position.x;
+    let hand_left_x = (frame.people[0].joints[8].position.x - pelvis_x) * -1;
+    let shoulder_left_x = (frame.people[0].joints[5].position.x - pelvis_x) * -1;
+    let hand_right_x = (frame.people[0].joints[15].position.x - pelvis_x) * -1;
+    let shoulder_right_x = (frame.people[0].joints[12].position.x - pelvis_x) * -1;
+
+    left_arm_in_line = ((hand_left_x - 50 < shoulder_left_x) || (hand_left_x + 50 > shoulder_left_x));
+    right_arm_in_line = ((hand_right_x - 50 < shoulder_right_x) || (hand_right_x + 50 > shoulder_right_x)); 
+    // had a check for left_and_right_arms_in_line before, but if left and right are in line alone they should already be in line with eachother
+    if (left_arm_in_line && right_arm_in_line) {
+      console.log("t-pose detected!");
+      return true;
+    }
+
+    return false;
+  },
+
   get_left_wrist_command: function (frame) {
-    var command = null;
+    let command = null;
     if (frame.people.length < 1) {
       return command;
     }
 
     // Normalize by subtracting the root (pelvis) joint coordinates
-    var pelvis_x = frame.people[0].joints[0].position.x;
-    var pelvis_y = frame.people[0].joints[0].position.y;
-    var pelvis_z = frame.people[0].joints[0].position.z;
-    var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
-    var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
-    var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
+    let pelvis_x = frame.people[0].joints[0].position.x;
+    let pelvis_y = frame.people[0].joints[0].position.y;
+    let pelvis_z = frame.people[0].joints[0].position.z;
+    let left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
+    let left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
+    let left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
 
     if (left_wrist_z < 100) {
       return command;
@@ -55,11 +109,11 @@ var frames = {
   }
 };
 
-var twod = {
+let twod = {
   socket: null,
 
   start: function() {
-    var url = "ws://" + host + "/twod";
+    let url = "ws://" + host + "/twod";
     twod.socket = new WebSocket(url);
     twod.socket.onmessage = function(event) {
       twod.show(JSON.parse(event.data));
