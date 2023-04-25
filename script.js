9667d4284718a5e10c5ee2e05b9971c67856ff81
Eric Sun <eric.sun.ecs92@yale.edu>
2023-04-25 14:10:44 -0400

    power pose done

--- script.js
+++ script.js
@@ -6,10 +6,20 @@ var poses = [
 	"pose4.jpg"
 ];
 
-// Set the initial pose image and page number
-var imageIndex = Math.floor(Math.random() * poses.length);
+// Set the number of poses to display
+var num_users = 3;
+
+// Set the initial pose images and page number
+var imageIndexes = [];
 var pageNumber = 1;
-document.getElementById("pose-image").src = poses[imageIndex];
+for (var i = 0; i < num_users; i++) {
+	var imageIndex = Math.floor(Math.random() * poses.length);
+	imageIndexes.push(imageIndex);
+	var poseImage = document.createElement("img");
+	poseImage.src = poses[imageIndex];
+	poseImage.classList.add("pose-image");
+	document.getElementById("poses-container").appendChild(poseImage);
+}
 document.getElementById("pose-page").textContent = "Pose " + pageNumber + "/3";
 
 // Start the timer to change poses
@@ -26,8 +36,15 @@ var timer = setInterval(function() {
 			clearInterval(timer);
 			window.location.href = "/";
 		} else {
-			imageIndex = Math.floor(Math.random() * poses.length);
-			document.getElementById("pose-image").src = poses[imageIndex];
+			for (var i = 0; i < num_users; i++) {
+				var imageIndex = Math.floor(Math.random() * poses.length);
+				while (imageIndexes.indexOf(imageIndex) !== -1) {
+					imageIndex = Math.floor(Math.random() * poses.length);
+				}
+				imageIndexes[i] = imageIndex;
+				var poseImage = document.getElementsByClassName("pose-image")[i];
+				poseImage.src = poses[imageIndex];
+			}
 			pageNumber++;
 			document.getElementById("pose-page").textContent = "Pose " + pageNumber + "/3";
 		}
