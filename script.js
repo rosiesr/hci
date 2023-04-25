a89784bbf4994ca17e1b3b7587e0e7256a70e9f2
Eric Sun <eric.sun.ecs92@yale.edu>
2023-04-25 16:57:27 -0400

    me first

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ var poses = [
 	"pose1.jpg",
 	"pose2.jpg",
 	"pose3.jpg",
-	"pose4.jpg"
+	"pose4.jpg",
+    "pose1.jpg",
 ];
 
 const queryParams = new URLSearchParams(window.location.search);
-
 // Set the number of poses to display
-var num_users = parseInt(queryParams.get('num_users'));
+let num_users = parseInt(queryParams.get('num_users'));
+console.log(num_users)
 
 // Set the initial pose images and page number
-var imageIndexes = [];
-var pageNumber = 1;
-for (var i = 0; i < num_users; i++) {
-	var imageIndex = Math.floor(Math.random() * poses.length);
+let imageIndexes = [];
+let pageNumber = 1;
+for (let i = 0; i < num_users; i++) {
+	let imageIndex = Math.floor(Math.random() * poses.length);
 	imageIndexes.push(imageIndex);
-	var poseImage = document.createElement("img");
+	let poseImage = document.createElement("img");
 	poseImage.src = poses[imageIndex];
 	poseImage.classList.add("pose-image");
 	document.getElementById("poses-container").appendChild(poseImage);
@@ -25,10 +26,10 @@ for (var i = 0; i < num_users; i++) {
 document.getElementById("pose-page").textContent = "Pose " + pageNumber + "/3";
 
 // Start the timer to change poses
-var countdownElement = document.getElementById("countdown");
-var countdown = 15;
-var poseChanges = 0;
-var timer = setInterval(function() {
+let countdownElement = document.getElementById("countdown");
+let countdown = 15;
+let poseChanges = 0;
+let timer = setInterval(function() {
 	countdown--;
 	countdownElement.textContent = countdown;
 	if (countdown === 0) {
@@ -38,14 +39,15 @@ var timer = setInterval(function() {
 			clearInterval(timer);
 			window.location.href = "7conclusion.html?app_state=7";
 		} else {
-			for (var i = 0; i < num_users; i++) {
-				var imageIndex = Math.floor(Math.random() * poses.length);
+			for (let i = 0; i < num_users; i++) {
+				let imageIndex = Math.floor(Math.random() * poses.length);
 				while (imageIndexes.indexOf(imageIndex) !== -1) {
 					imageIndex = Math.floor(Math.random() * poses.length);
 				}
 				imageIndexes[i] = imageIndex;
-				var poseImage = document.getElementsByClassName("pose-image")[i];
+				let poseImage = document.getElementsByClassName("pose-image")[i];
 				poseImage.src = poses[imageIndex];
+
 			}
 			pageNumber++;
 			document.getElementById("pose-page").textContent = "Pose " + pageNumber + "/3";
